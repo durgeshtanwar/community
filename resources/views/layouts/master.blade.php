b3ea18ef7a6e599881d452356055882e1d539289
Durgesh Tanwar <tanwardurgesh@gmail.com>
2019-10-03 11:38:18 +0530

    adding vue router

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -229,7 +229,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <div class="content-wrapper">
     
       <!-- Main content -->
-    
+    <div class="content">
+      <div class="container-fluid">
+        <router-view></router-view>
+      </div> <!--/container fludi-->
+    </div> 
+    {{-- content --}}
 
            
   </div>
